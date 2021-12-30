4abc271669331a4d372be58c718fea7ad44f3bc8
yuhki666 <yuhki666@gmail.com>
2021-12-30 22:26:45 +0900

    add quote

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import { useRouter } from "next/router";
+import useTranslation from "next-translate/useTranslation";
 export default function Index() {
+  const { t } = useTranslation();
+  let router = useRouter();
   return (
     <Layout>
-      <h1>HHHH</h1>
+      <h1>{t("common:quote")}</h1>
+      <h2>{t("common:author")}</h2>
     </Layout>
   );
 }
